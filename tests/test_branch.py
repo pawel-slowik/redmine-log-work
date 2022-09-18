e62c2ec9c24fe7699ee6cf67104f384f8d820885
Paweł Słowik <pawel.slowik@zoho.com>
2022-09-18 16:40:23 +0200

    Add tests for branch name parsing

--- tests/test_branch.py
+++ tests/test_branch.py
@@ -1,4 +1,5 @@
-from redmine_log_work import issue_id_from_branch_name
+import subprocess
+from redmine_log_work import issue_id_from_branch_name, branch_name_from_directory
 
 
 def test_id_as_suffix() -> None:
@@ -15,3 +16,26 @@ def test_id_with_type() -> None:
 
 def test_id_as_prefix() -> None:
     assert issue_id_from_branch_name("123-baz-qux") == "123"
+
+
+def test_name_can_be_read() -> None:
+    name = branch_name_from_directory(".")
+    assert name
+    assert isinstance(name, str)
+
+
+def test_name_is_parsed_correctly(mocker) -> None:
+    git_info = (
+        b"# branch.oid b57a1c3bc3c7c8de6792863f4b91f155c686c419\0"
+        b"# branch.head master\0"
+        b"1 .M N... 100644 100644 100644 "
+        b"2bdaa2bde719e7781828c7fa6da29172ac26ea16 "
+        b"2bdaa2bde719e7781828c7fa6da29172ac26ea16 "
+        b"tests/test_branch.py\0"
+        b"? .coverage\0"
+        b"? README.md\0"
+    )
+    process = subprocess.CompletedProcess([], 0, stdout=git_info)
+    mocker.patch("subprocess.run", return_value=process)
+    name = branch_name_from_directory(".")
+    assert name == "master"
